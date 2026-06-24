96d9fce29e5b6f1363bd9f7e163840cf398bdf2e
ttdms <ttdms@vip.qq.com>
2026-06-24 23:40:48 +0800

    重构所有

--- src-tauri/src/main.rs
+++
@@ -1,5 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    clash_mg_lib::run()
-}
--- src-tauri/src/tray/mod.rs
+++
@@ -1,6 +0,0 @@
-use tauri::AppHandle;
-
-pub fn setup_tray(_app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
-    // Tray menus are platform-specific enough to keep behind this module.
-    Ok(())
-}
